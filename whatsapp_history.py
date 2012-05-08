fae3e621b55456855ab2fa35abc591a8241ab1b9
wiggin15 <wiggin15@yahoo.com>
2012-05-08 17:30:39 +0300

    all backup parsing functions are now in "BackupExtractor", which can retrieve a physical filename based on logical filename

--- whatsapp_history.py
+++ whatsapp_history.py
@@ -8,64 +8,74 @@ import datetime
 import mbdb
 import chat_output
 
-def backup_time(backup_dir):
-	# time of backup is stored in info.plist, which is in xml format
-	info_file = os.path.join(backup_dir, "Info.plist")
-	info_data = open(info_file, "r").read()
-	match_obj = re.search("<date>(.*?)</date>", info_data)
-	if match_obj is None:
-		return None		# TODO if this happens the script will crash
-	time_str = match_obj.group(1)
-	res = datetime.datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ")
-	return res
+class BackupExtractor():
+	def _backup_time(self, backup_dir):
+		# time of backup is stored in info.plist, which is in xml format
+		info_file = os.path.join(backup_dir, "Info.plist")
+		info_data = open(info_file, "r").read()
+		match_obj = re.search("<date>(.*?)</date>", info_data)
+		if match_obj is None:
+			return None		# TODO if this happens the script will crash
+		time_str = match_obj.group(1)
+		res = datetime.datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ")
+		return res
 
-def get_backup_folder():
-	# return latest backup folder
-	# don't be confused between:
-	# - backups_folder (root dir of backups)
-	# - backup_folders (list of all folders of backups)
-	# - backup_folder (result)
+	def _get_backup_folder(self):
+		# return latest backup folder
+		# don't be confused between:
+		# - backups_folder (root dir of backups)
+		# - backup_folders (list of all folders of backups)
+		# - backup_folder (result)
 
-	backups_folder = None
-	if sys.platform == "win32":
-		backups_folder = os.path.expandvars(r"%appdata%\Apple Computer\MobileSync\Backup")
-	elif sys.platform == "darwin":
-		backups_folder = os.path.expanduser("~/Library/Application Support/MobileSync/Backup")
-	else:
-		print("Unsupported system: %s" % sys.platform)
-		return None
+		backups_folder = None
+		if sys.platform == "win32":
+			backups_folder = os.path.expandvars(r"%appdata%\Apple Computer\MobileSync\Backup")
+		elif sys.platform == "darwin":
+			backups_folder = os.path.expanduser("~/Library/Application Support/MobileSync/Backup")
+		else:
+			print("Unsupported system: %s" % sys.platform)
+			return None
 
-	backup_folders = os.listdir(backups_folder)
-	backup_folders = [os.path.join(backups_folder, d) for d in backup_folders]
-	backup_folders = [d for d in backup_folders if os.path.isdir(d)]
+		backup_folders = os.listdir(backups_folder)
+		backup_folders = [os.path.join(backups_folder, d) for d in backup_folders]
+		backup_folders = [d for d in backup_folders if os.path.isdir(d)]
 
-	backup_folders_with_times = [(d, backup_time(d)) for d in backup_folders]
-	backup_folders_with_times.sort(reverse=True, key=lambda k: k[1])
+		backup_folders_with_times = [(d, self._backup_time(d)) for d in backup_folders]
+		backup_folders_with_times.sort(reverse=True, key=lambda k: k[1])
 
-	backup_folder = backup_folders_with_times[0][0]
+		backup_folder = backup_folders_with_times[0][0]
 
-	return backup_folder
+		return backup_folder
+	
+	def __init__(self):
+		backup_folder = self._get_backup_folder()
+		if backup_folder is None:
+			raise Exception("Could not find backup folder")
+
+		mbdb_file = os.path.join(backup_folder, "Manifest.mbdb")
+
+		files_in_backup = mbdb.process_mbdb_file(mbdb_file)
+
+		# file index: map domain+filename to physical file in backup directory
+		self.file_index = {}
+		for f in files_in_backup:
+			domain = str(f['domain'], "ascii")
+			filename = str(f['filename'], "ascii")
+			file_path = os.path.join(backup_folder, str(f['fileID']))
+			self.file_index[(domain, filename)] = file_path
+
+	def get_file_path(self, domain, filename):
+		return self.file_index.get((domain, filename), None)
 
 def main():
-	backup_folder = get_backup_folder()
-	if backup_folder is None:
-		print("Could not find backup folder")
-		sys.exit()
+	backup_extractor = BackupExtractor()
 
-	mbdb_file = os.path.join(backup_folder, "Manifest.mbdb")
-
-	files_in_backup = mbdb.process_mbdb_file(mbdb_file)
-
-	whatsapp_chat_file = None
-	for f in files_in_backup:
-		if f['domain'] == b"AppDomain-net.whatsapp.WhatsApp" and f['filename'] == b"Documents/ChatStorage.sqlite":
-			whatsapp_chat_file = f['fileID']
+	whatsapp_chat_file = backup_extractor.get_file_path("AppDomain-net.whatsapp.WhatsApp", "Documents/ChatStorage.sqlite")
 
 	if whatsapp_chat_file is None:
 		print("Could not find WhatsApp Chat file")
 		sys.exit()
 
-	whatsapp_chat_file = os.path.join(backup_folder, str(whatsapp_chat_file))
 	shutil.copy(whatsapp_chat_file, chat_output.CHAT_STORAGE_FILE)
 
 	chat_output.main()
